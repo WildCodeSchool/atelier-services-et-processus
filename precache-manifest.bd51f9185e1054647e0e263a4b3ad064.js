ef2f417c658c99205d5e38b200570fb8e55c7fe5
dominique colleville <dominique.colleville@wildcodeschool.com>
2022-07-20 14:43:56 +0200

    Updates

---
+++ precache-manifest.bd51f9185e1054647e0e263a4b3ad064.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69e2a0f3933f490fef1b341764312cf",
+    "url": "/atelier-services-et-processus/index.html"
+  },
+  {
+    "revision": "1a18d207c39faadbf076",
+    "url": "/atelier-services-et-processus/static/js/2.3c09b51d.chunk.js"
+  },
+  {
+    "revision": "65cdb05552a01afd0e99",
+    "url": "/atelier-services-et-processus/static/js/main.f37669fe.chunk.js"
+  },
+  {
+    "revision": "0edde0c08348ad83ea03",
+    "url": "/atelier-services-et-processus/static/js/runtime~main.0390c666.js"
+  }
+]);
